a51c8e9ef051c2fdcffc05cee37366a6a38b973b
viet01042001 <nvviet142001@gmail.com>
2022-12-06 17:11:19 +0700

    successfully read value

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+
+import { ref, onValue } from 'firebase/database';
+import { db } from './src/firebaseConfig';
+
 
 export default function App() {
+
+  var data = ""
+  var dataKeys = ""
+  var dataArray = ""
+
+  const starCountRef = ref(db, 'CO2_quality/');
+  onValue(starCountRef, (snapshot) => {
+    data = snapshot.val();
+  });
+  dataKeys = Object.keys(data);
+  dataArray = dataKeys.map(key => [data[key]]);
+  
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        
+      <TouchableOpacity style={styles.box}>
+          <Text> Nồng độ CO2 là:  </Text>
+      </TouchableOpacity>
+
+      <Text> {dataArray} </Text>
+
     </View>
   );
 }
@@ -15,6 +37,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
   },
+  box: {
+    margin: 2,
+    borderWidth: 0.5,
+    width: 200,
+    padding: 2,
+    height: 200,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
