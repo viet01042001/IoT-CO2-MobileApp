41d8f6f7fabb47336f715f438bb7dc0cacf58ee4
viet01042001 <nvviet142001@gmail.com>
2022-12-06 21:10:55 +0700

    realtime receive value done

--- App.js
+++ App.js
@@ -1,22 +1,32 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { TouchableOpacity } from 'react-native';
 
+import { useState, useEffect } from 'react';
+
 import { ref, onValue } from 'firebase/database';
 import { db } from './src/firebaseConfig';
 
 
 export default function App() {
 
-  var data = ""
-  var dataKeys = ""
-  var dataArray = ""
+  const [data, setData] = useState({});
+  const [dataKeys, setDataKeys] = useState([]);
+  const [dataArray, setDataArray] = useState([]);
 
-  const starCountRef = ref(db, 'CO2_quality/');
-  onValue(starCountRef, (snapshot) => {
-    data = snapshot.val();
-  });
-  dataKeys = Object.keys(data);
-  dataArray = dataKeys.map(key => [data[key]]);
+  useEffect(() => {
+    const starCountRef = ref(db, 'CO2_quality/');
+    onValue(starCountRef, (snapshot) => {
+      setData(snapshot.val());
+    });
+  }, [])
+
+  useEffect(() => {
+    setDataKeys(Object.keys(data));
+  }, [data])
+
+  useEffect(() => {
+    setDataArray(dataKeys.map(key => [data[key]]));
+  }, [dataKeys])
   
   return (
     
@@ -26,7 +36,13 @@ export default function App() {
           <Text> Nồng độ CO2 là:  </Text>
       </TouchableOpacity>
 
-      <Text> {dataArray} </Text>
+      <Text>
+        {dataArray.map((dataArrayI, index) => {        // -> đoạn này in ra cho dễ nhìn thôi
+          return <Text key={index}>
+            {`${dataArrayI}    `}
+          </Text>
+        })}
+      </Text>
 
     </View>
   );
